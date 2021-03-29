4f503489bc3c71ba7b39bd636a87c838793a602e
munishaboora <78922862+munishaboora@users.noreply.github.com>
2021-03-29 16:44:35 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -23,15 +23,41 @@ function addWord(request, response) {
     var data = request.params;
     var word = data.word;
     var score = Number(data.score);
-    words[word] = score;
-    var reply = {
-        msg: "Thanks for visiting this site!"
+    var reply;
+    if (!score) {
+        score = 5;
+        var reply = {
+            msg: "Score is required!"
+        }
+    } else {
+        words[word] = score;
+        var reply = {
+            msg: "Thanks for visiting this site!"
+        }
     }
+    response.send(reply);
 }
-response.send(reply);
-
 
 app.get('/all', sendAll);
 function sendAll(request, response) {
     response.send(words);
+}
+
+app.get('/search/:word/', searchWord);
+
+function searchWord(request, response) {
+    var word = request.params.word;
+    var reply;
+    if (words[word]) {
+        reply = {
+            status: "found",
+            "word": word,
+            score: words[word]
+        }
+    } else {
+        reply = {
+            status: "not found",
+            "word": word,
+        }
+    }
 }
